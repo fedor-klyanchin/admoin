7c492ca9cefca1686621fa90420f4a0fe5303419
Fedor Klyanchin <46520425+fedor-klyanchin@users.noreply.github.com>
2023-07-22 21:16:59 +0300

    add test

---
+++ src/test/java/com/admoin/action/type/database/query/SelectTest.java
@@ -0,0 +1,62 @@
+package com.admoin.action.type.database.query;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.admoin.Host;
+import com.admoin.Log;
+import com.admoin.action.Action;
+import com.admoin.action.type.Type;
+
+public class SelectTest {
+    private String tablePath = "host";
+    private String tableName = Type.getTableName(tablePath);
+    private Select actionSelect = new Select(tablePath);
+    private ConcurrentHashMap<Integer, Select> actionDataBaseQuerySelectMap = new ConcurrentHashMap<>();
+    private Action action = new Action(1, 1, 0);
+
+    @Test
+    @BeforeClass
+    public void setUp() throws Exception {
+        Log.create();
+        Host.getProperties();
+        actionDataBaseQuerySelectMap.put(1, actionSelect);
+    }
+
+    @Test(groups = { "Select" })
+    public void newAction() {
+        AssertJUnit.assertTrue(tablePath.equals(actionSelect.getResultTablePath()));
+    }
+
+    @Test(groups = { "Select" })
+    public void newActionMap() {
+        AssertJUnit.assertTrue(actionDataBaseQuerySelectMap.get(1).getResultTablePath().equals(tablePath));
+    }
+
+    @Test(groups = { "Select" })
+    public void getTableName() {
+        AssertJUnit.assertTrue(actionSelect.getResultTableName().equals("host"));
+    }
+
+    @Test
+    void testGetFromDataBase() {
+
+    }
+
+    @Test
+    void testGetResultTableName() {
+        AssertJUnit.assertTrue(actionSelect
+            .getResultTableName()
+            .equals(tableName));
+    }
+
+    @Test
+    void testGetResultTablePath() {
+        AssertJUnit.assertTrue(actionSelect
+            .getResultTablePath()
+            .equals(tablePath));
+    }
+}
