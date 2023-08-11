8e53de79c0becee3f67179a994f6284397455cde
Fedor Klyanchin <46520425+fedor-klyanchin@users.noreply.github.com>
2023-08-12 00:36:44 +0300

    <version>2.0.11</version>

--- src/main/java/com/admoin/App.java
+++ src/main/java/com/admoin/App.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 import com.admoin.action.type.file.Start;
 
 public class App {
-    public static final String APP_VERSION = "2.0.10";
+    public static final String APP_VERSION = "2.0.11";
 
     static Map<String, String> config = new HashMap<>();
     static boolean oldVersionApp;
